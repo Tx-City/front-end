cea576f5f5f6f32634b0235a1eb3978fc9ce7a8d
Mohit Chandel <devmohit93@gmail.com>
2024-08-07 17:03:22 +0530

    Fix: created wallet addresses dynamic

--- src/data/donateAd.js
+++ src/data/donateAd.js
@@ -1,7 +1,40 @@
-export const donateData = {
-    key:"donate-ad",
-    title:"Contribute to Txcity!",
-    html:	`<div class="ta-l">
+export const donateData = (ticker) => {
+	console.log("ticker", ticker);
+
+	let walletAddress;
+	switch (ticker) {
+		case "ETH":
+			walletAddress = "0x01";
+			break;
+		case "BTC":
+			walletAddress = "0x02";
+			break;
+		case "XMR":
+			walletAddress = "0x03";
+			break;
+		case "BCH":
+			walletAddress = "0x04";
+			break;
+		case "LTC":
+			walletAddress = "0x05";
+			break;
+		case "ARBI":
+			walletAddress = "0x06";
+			break;
+		case "CELO":
+			walletAddress = "0x07";
+			break;
+		case "LUKSO":
+			walletAddress = "0x08";
+			break;
+		default:
+			walletAddress = "0x09";
+	}
+
+	return {
+		key: "donate-ad",
+		title: "Contribute to Txcity!",
+		html: `<div class="ta-l">
     <span class="t-yellow">Txcity citizens!</span>
     <br />
     Now you can contribute to the development of Txcity, your donations will help to develop features.						
@@ -11,16 +44,13 @@ export const donateData = {
     <b>Donation address</b>
     <ul>
     <li>
-    <span class="t-yellow">ETH, Polygon or any EVM blockchain:</span
+    <span class="t-yellow">Wallet address fro ${ticker}:</span
     <br/>
-    0x93905fd3f9b8732015f2b3Ca6c16Cbcb60ECf895</li>
-    <li>
-    <span class="t-yellow">Bitcoin Cash (BCH address) :</span>
-    <br/> 
-    bitcoincash:qrcxf720hhrfp7ush0lpsusu5309l7z8gc5ytpsplk
-    </li>						
+    ${walletAddress}</li>
+    						
     </ul>									
     </div>								
     </div>
-    `
-}
+    `,
+	};
+};
